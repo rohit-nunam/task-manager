2d385517b87d6b8a8a6adf4fa0ae58b3eb1b95fb
rohit-nunam <rohit.bhandari@nunam.com>
2025-08-02 14:22:18 +0530

    added redis config

---
+++ src/main/java/com/rohit/task_manager/config/RedisConfig.java
@@ -0,0 +1,18 @@
+package com.rohit.task_manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10))
+            .disableCachingNullValues();
+    }
+}
